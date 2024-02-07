cb2021e2b4bc7e1f613e027898de897a00bb8935
Sergey Untilov <s.untilov@gmail.com>
2024-02-07 18:52:54 +0200

    Tests for Laws controller

--- apps/api/src/resources/laws/laws.controller.spec.ts
+++ apps/api/src/resources/laws/laws.controller.spec.ts
@@ -1,8 +1,9 @@
+import { randCountry } from '@ngneat/falso';
 import { Test, TestingModule } from '@nestjs/testing';
 import { LawsController } from './laws.controller';
 import { LawsService } from './laws.service';
 import { getRepositoryToken } from '@nestjs/typeorm';
-import { repositoryMockFactory } from '@repo/utils';
+import { createMockLaw, repositoryMockFactory } from '@repo/utils';
 import { Law } from './entities/law.entity';
 
 describe('LawsController', () => {
@@ -28,5 +29,35 @@ describe('LawsController', () => {
     it('should be defined', () => {
         expect(controller).toBeDefined();
         expect(service).toBeDefined();
+    });
+
+    it('should create a law', async () => {
+        const law = createMockLaw();
+        jest.spyOn(service, 'create').mockReturnValue(Promise.resolve(law));
+        const res = await controller.create({ name: law.name });
+        expect(res).toStrictEqual(law);
+    });
+
+    it('should get law details', async () => {
+        const law = createMockLaw();
+        jest.spyOn(service, 'findOne').mockReturnValue(Promise.resolve(law));
+        const res = await controller.findOne(law.id.toString());
+        expect(res).toStrictEqual(law);
+    });
+
+    it('should update a law', async () => {
+        const law = createMockLaw();
+        const newName = randCountry();
+        const updatedLaw = { ...law, name: newName };
+        jest.spyOn(service, 'update').mockReturnValue(Promise.resolve(updatedLaw));
+        const res = await controller.update(law.id.toString(), { name: newName });
+        expect(res).toStrictEqual(updatedLaw);
+    });
+
+    it('should remove a law', async () => {
+        const law = createMockLaw();
+        jest.spyOn(service, 'remove').mockReturnValue(Promise.resolve(law));
+        const res = await controller.remove(law.id.toString());
+        expect(res).toStrictEqual(law);
     });
 });
