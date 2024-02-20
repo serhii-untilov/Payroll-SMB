74c43ff1870e9ef832a3c9aefb17669d9759675d
Sergey Untilov <s.untilov@gmail.com>
2024-02-20 10:58:26 +0200

    Front-end refresh token

--- apps/web/src/services/auth-header.ts
+++ apps/web/src/services/auth-header.ts
@@ -1,13 +1,11 @@
-export default function authHeader() {
-    const userStr = localStorage.getItem('user');
-    let user = null;
-    if (userStr) user = JSON.parse(userStr);
+import { getUserAccessToken } from './token.service';
 
-    if (user && user.accessToken) {
-        // return { 'x-access-token': user.accessToken };
-        return { Authorization: `Bearer ${user.accessToken}` };
+export default function authHeader() {
+    const accessToken = getUserAccessToken();
+
+    if (accessToken) {
+        return { Authorization: `Bearer ${accessToken}` };
     } else {
-        // return { 'x-access-token': null };
         return { Authorization: null };
     }
 }
--- apps/web/src/services/auth.service.ts
+++ apps/web/src/services/auth.service.ts
@@ -1,12 +1,13 @@
 import { IAuth, ICreateUser, ITokens, IUser } from '@repo/shared';
 import { api } from '../api';
 import authHeader from './auth-header';
+import { saveUserTokens, removeUserTokens } from './token.service';
 
 export const registerUser = async (params: ICreateUser): Promise<ITokens> => {
     const response = await api.post('/api/auth/register', params);
     const tokens: ITokens = response.data;
     if (tokens.accessToken) {
-        localStorage.setItem('user', JSON.stringify(tokens));
+        saveUserTokens(tokens);
     }
     return tokens;
 };
@@ -15,22 +16,16 @@ export const loginUser = async (params: IAuth): Promise<ITokens> => {
     const response = await api.post('/api/auth/login', params);
     const tokens: ITokens = response.data;
     if (tokens.accessToken) {
-        localStorage.setItem('user', JSON.stringify(tokens));
+        saveUserTokens(tokens);
     }
     return tokens;
 };
 
 export const logoutUser = async () => {
     await api.get('/api/auth/logout', { headers: authHeader() });
-    localStorage.removeItem('user');
+    removeUserTokens();
 };
 
 export const getCurrentUser = async (): Promise<IUser | null> => {
     return await api.get('/api/auth/user', { headers: authHeader() });
-};
-
-export const getUserTokens = (): ITokens | null => {
-    const userStr = localStorage.getItem('user');
-    if (userStr) return JSON.parse(userStr);
-    return null;
 };
