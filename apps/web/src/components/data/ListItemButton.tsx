51398b0f4d3b68bed766ee3c5b89e33965f22668
Sergey Untilov <s.untilov@gmail.com>
2024-02-21 20:17:08 +0200

    Profile page

--- apps/web/src/components/data/ListItemButton.tsx
+++ apps/web/src/components/data/ListItemButton.tsx
@@ -23,10 +23,18 @@ export function ListItemButton(props: ListItemButtonProps) {
                 onClick={onClick}
                 sx={{
                     '&.active': {
-                        // color: blue[700],
                         bgcolor: grey[300],
+                        borderRadius: '5px',
+                        py: [0.5],
+                        my: [0.3],
                     },
-                    // color: 'black',
+                    '&:hover': {
+                        borderRadius: '5px',
+                        py: [0.5],
+                        my: [0.3],
+                    },
+                    py: [0.5],
+                    my: [0.3],
                 }}
             >
                 {icon ? <ListItemIcon>{icon}</ListItemIcon> : null}
--- apps/web/src/components/layout/PageLayout.tsx
+++ apps/web/src/components/layout/PageLayout.tsx
@@ -1,4 +1,5 @@
 import { Box, Typography } from '@mui/material';
+import { grey } from '@mui/material/colors';
 import { FC, ReactNode } from 'react';
 
 interface PageLayoutProps {
@@ -8,32 +9,14 @@ interface PageLayoutProps {
 
 const PageLayout: FC<PageLayoutProps> = ({ title, children }) => {
     return (
-        <>
-            <Box
-                sx={{
-                    height: 48,
-                    py: [1],
-                    ml: [5],
-                }}
-            >
-                <Box
-                    sx={{
-                        flexGrow: 1,
-                    }}
-                >
-                    <Typography
-                        component="h1"
-                        variant="h5"
-                        // color="primary.main"
-                        noWrap
-                        sx={{}}
-                    >
-                        {title}
-                    </Typography>
-                </Box>
-                {children}
+        <Box sx={{ height: 48, py: [1], ml: [5] }}>
+            <Box sx={{ flexGrow: 1 }}>
+                <Typography component="h1" variant="h5" color={grey[800]} noWrap>
+                    {title}
+                </Typography>
             </Box>
-        </>
+            {children}
+        </Box>
     );
 };
 
