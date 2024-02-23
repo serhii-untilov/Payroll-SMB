18a0c416cb11ef5b939c883748fca3ac5c4aae18
Sergey Untilov <s.untilov@gmail.com>
2024-02-23 09:24:37 +0200

    Button color

--- apps/web/src/components/form/FormButton.tsx
+++ apps/web/src/components/form/FormButton.tsx
@@ -2,4 +2,5 @@ import { Button, ButtonProps, styled } from '@mui/material';
 
 export const FormButton = styled(Button)<ButtonProps>(({ theme }) => ({
     textTransform: 'none',
+    opacity: 0.85,
 }));
