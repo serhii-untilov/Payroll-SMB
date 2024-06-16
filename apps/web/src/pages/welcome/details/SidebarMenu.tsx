c50984f0cf641e22ec3fc90c5417e0def3fdc27c
Sergey Untilov <s.untilov@gmail.com>
2024-06-16 07:43:51 +0300

    chore: welcome page sidebar menu

--- apps/web/src/pages/welcome/details/SidebarMenu.tsx
+++ apps/web/src/pages/welcome/details/SidebarMenu.tsx
@@ -74,7 +74,7 @@ export function SidebarMenu(props: Props) {
                 onClick: switchThemeMode,
             },
         ],
-        [t, props],
+        [t, props, locale, switchThemeMode, themeMode],
     );
 
     return (
