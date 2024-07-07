1448326ee166fed0ed8736929ff7b778186d43ff
Sergey Untilov <s.untilov@gmail.com>
2024-07-07 10:12:28 +0300

    build: swagger codegen api for front-end

---
+++ apps/api/src/main-swagger.ts
@@ -0,0 +1,39 @@
+import { VersioningType } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app/app.module';
+import metadata from './metadata';
+import path from 'path';
+import { writeFileSync } from 'fs';
+
+async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
+    const globalPrefix = 'api';
+    app.setGlobalPrefix(globalPrefix);
+    const configService = app.get(ConfigService);
+    // set up versioning
+    app.enableVersioning({ type: VersioningType.URI, prefix: 'v' });
+    // TODO - revisit and secure this!
+    // app.enableCors({ origin: '*' });
+    const title = configService.get<string>('app.title');
+
+    // handle Swagger
+    const config = new DocumentBuilder()
+        .setTitle(`${title} REST API`)
+        .setVersion('1.0')
+        .addBearerAuth()
+        .build();
+    await SwaggerModule.loadPluginMetadata(metadata);
+    const document = SwaggerModule.createDocument(app, config);
+    const apiVersion = 'v1';
+    const apiFileName = 'swagger';
+    SwaggerModule.setup(`${globalPrefix}/${apiVersion}`, app, document, {
+        jsonDocumentUrl: `${globalPrefix}/${apiVersion}/${apiFileName}.json`,
+        yamlDocumentUrl: `${globalPrefix}/${apiVersion}/${apiFileName}.yaml`,
+    });
+    const outputPath = path.resolve(process.cwd(), 'swagger.json');
+    writeFileSync(outputPath, JSON.stringify(document), { encoding: 'utf8' });
+    await app.close();
+}
+bootstrap();
