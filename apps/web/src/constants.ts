5be38cd304a9091bf987bfa43823b4fbc45d3e2e
Sergey Untilov <s.untilov@gmail.com>
2024-08-04 20:42:26 +0300

    refactor: add some constants

--- apps/web/src/constants.ts
+++ apps/web/src/constants.ts
@@ -1,3 +1,5 @@
+export const Author = 'Serhii Untilov';
+export const Website = 'https://untilov.com.ua';
 export const GitHubLink = 'https://github.com/serhii-untilov/Payroll-SMB';
 export const LinkedInLink = 'https://www.linkedin.com/in/serhiiuntilov/';
 export const FacebookLink = 'https://www.facebook.com/s.untilov/';
